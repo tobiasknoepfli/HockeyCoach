6ced169bb6b76447bef71179c55fc9c6a465d722
Tobias Knöpfli <113889152+tobiasknoepfli@users.noreply.github.com>
2023-07-20 22:49:33 +0200

    added constructors, added getters and setters

--- src/main/java/hockeycoach/mainClasses/Drill.java
+++ src/main/java/hockeycoach/mainClasses/Drill.java
@@ -11,5 +11,66 @@ public class Drill {
     private Boolean station;
     private ArrayList<String> tags;
 
+    public Drill(String name, String category, int difficulty, String participation) {
+        this.name = name;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.participation = participation;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getParticipation() {
+        return participation;
+    }
+
+    public void setParticipation(String participation) {
+        this.participation = participation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getStation() {
+        return station;
+    }
+
+    public void setStation(Boolean station) {
+        this.station = station;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
 }
--- src/main/java/hockeycoach/mainClasses/Player.java
+++ src/main/java/hockeycoach/mainClasses/Player.java
@@ -21,4 +21,145 @@ public class Player {
     private ArrayList<String> team;
     private Path photoPath;
     private String role;
+
+    public Player(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getaLicence() {
+        return aLicence;
+    }
+
+    public void setaLicence(String aLicence) {
+        this.aLicence = aLicence;
+    }
+
+    public String getbLicence() {
+        return bLicence;
+    }
+
+    public void setbLicence(String bLicence) {
+        this.bLicence = bLicence;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public ArrayList<Integer> getJersey() {
+        return jersey;
+    }
+
+    public void setJersey(ArrayList<Integer> jersey) {
+        this.jersey = jersey;
+    }
+
+    public ArrayList<String> getPosition() {
+        return position;
+    }
+
+    public void setPosition(ArrayList<String> position) {
+        this.position = position;
+    }
+
+    public ArrayList<String> getStrength() {
+        return strength;
+    }
+
+    public void setStrength(ArrayList<String> strength) {
+        this.strength = strength;
+    }
+
+    public ArrayList<String> getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(ArrayList<String> weakness) {
+        this.weakness = weakness;
+    }
+
+    public ArrayList<String> getTeam() {
+        return team;
+    }
+
+    public void setTeam(ArrayList<String> team) {
+        this.team = team;
+    }
+
+    public Path getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(Path photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
